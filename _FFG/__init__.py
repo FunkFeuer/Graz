2514994c86ac7f81b8658df3c76b7b1b4be15566
Michael Bauer <mihi@lo-res.org>
2014-09-01 13:29:29 +0200

    added the _FFG customizations

---
+++ _FFG/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012-2013 Mag. Christian Tanzer All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# #*** <License> ************************************************************#
+# This module is part of the package FFG.
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this module. If not, see <http://www.gnu.org/licenses/>.
+# #*** </License> ***********************************************************#
+#
+#++
+# Name
+#    FFG.__init__
+#
+# Purpose
+#    Package defining the Funkfeuer specific classes on top of the common node model
+#
+# Revision Dates
+#     9-Jul-2014 (CT) Creation
+#    01-Sep-2014 (MB) Moved to FFG
+#    ««revision-date»»···
+#--
+
+from   __future__  import absolute_import, division, print_function, unicode_literals
+
+from   _MOM                 import MOM
+import _MOM.Derived_PNS
+
+_desc_ = """
+Funkfeuer specific classes on top of the common node model.
+"""
+
+FFG = MOM.Underived_PNS ()
+
+### __END__ FFG.__init__
